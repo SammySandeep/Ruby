d9b2a5a0eb63736f8e189d28183df11f0b0ff0a9
sandeep182 <s.sandeep@kloctechnologies.co.in>
2023-07-28 14:18:01 +0530

    chore: changed proc.rb file

--- Ruby-Basics/proc.rb
+++ Ruby-Basics/proc.rb
@@ -1,2 +1,20 @@
-name=proc{|first,last|first*5+"...."+last}
-p name["sandeep","kumar"]
+name = Proc.new{|first,last|first*5+"...."+last}
+p name["jordan","kumar"]
+
+full_name = Proc.new do |first|
+    first * 5
+end
+
+p full_name["sandeep"]
+
+
+last_name = lambda{|first,last|first*5+"...."+last}
+p last_name["sandeep","kumar"]
+
+def my_method
+    x = lambda{return}
+    x.call
+    p "Text from within the method"
+end
+
+my_method
